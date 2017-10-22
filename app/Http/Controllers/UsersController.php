0efa51e563fa80b816523536c75ff24b2e62f648
qa13979768899ws <32052181+qa13979768899ws@users.noreply.github.com>
2017-10-22 14:59:45 +0800

    Update UsersController.php

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -47,7 +47,7 @@ class UsersController extends Controller
 
         $this->sendEmailConfirmationTo($user);
         session()->flash('success', '验证邮件已发送到你的注册邮箱上，请注意查收。');
-        return redirect('/');
+        return redirect('www.baidu.com');
     }
 
     public function index()
